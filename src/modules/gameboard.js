de901b1f526d99967bf314c800628f66cc249566
lucafisc <lucafischer@fastmail.com>
2022-07-31 23:24:47 +0200

    Test to check if ship is too long for cell or column

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -9,10 +9,13 @@ export const gameboard = () => {
   const placeShip = (orientation, shipName, column, row) => {
     const newShip = ship(shipName);
     const shipLength = newShip.getLength();
-
-    for (let i = 0; i < shipLength; i++) {
-      board[column][row]["ship"] = true;
-      row += 1;
+    switch (orientation) {
+      case "vertical":
+        placeVertical(shipLength, board, column, row);
+        break;
+      case "horizontal":
+        placeHorizontal(shipLength, board, column, row);
+        break;
     }
   };
 
@@ -27,6 +30,35 @@ export const gameboard = () => {
   };
 };
 
+function placeVertical(shipLength, board, column, row) {
+  let index = row;
+  if (index + shipLength > 10) {
+    return;
+  } else {
+    for (let i = 0; i < shipLength; i++) {
+      board[column][index]["ship"] = true;
+      index += 1;
+    }
+  }
+}
+
+function placeHorizontal(shipLength, board, column, row) {
+  const alphabet = makeAlphabet();
+  let index = alphabet.findIndex((i) => i === column);
+  console.log(index);
+  console.log(alphabet[index]);
+  console.log(shipLength);
+  console.log(index + shipLength);
+  if (index + shipLength > 10) {
+    return;
+  } else {
+    for (let i = 0; i < shipLength; i++) {
+      board[alphabet[index]][row]["ship"] = true;
+      index += 1;
+    }
+  }
+}
+
 function newBoard() {
   let cell = {
     hit: false,
@@ -39,11 +71,15 @@ function newBoard() {
     column[element] = Object.assign({}, cell);
   });
 
-  const alpha = Array.from(Array(10)).map((e, i) => i + 65);
-  const alphabet = alpha.map((x) => String.fromCharCode(x));
+  const alphabet = makeAlphabet();
   const board = {};
   alphabet.forEach((element, index) => {
     board[element] = JSON.parse(JSON.stringify(column));
   });
   return board;
+}
+function makeAlphabet() {
+  const alpha = Array.from(Array(10)).map((e, i) => i + 65);
+  const alphabet = alpha.map((x) => String.fromCharCode(x));
+  return alphabet;
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -11,23 +11,50 @@ test("test receive attack", () => {
 test("place ships vertically", () => {
   let myBoard = gameboard();
   const shipName = "destroyer";
-  myBoard.placeShip("vertical", shipName, "B", 3);
+  myBoard.placeShip("vertical", shipName, "A", 3);
   let board = myBoard.getBoard();
 
-  expect(board["B"]["2"]["ship"]).toBeFalsy();
-  expect(board["B"]["3"]["ship"]).toBeTruthy();
-  expect(board["B"]["4"]["ship"]).toBeTruthy();
-  expect(board["B"]["5"]["ship"]).toBeTruthy();
-  expect(board["B"]["6"]["ship"]).toBeFalsy();
+  expect(board["A"]["2"]["ship"]).toBeFalsy();
+  expect(board["A"]["3"]["ship"]).toBeTruthy();
+  expect(board["A"]["4"]["ship"]).toBeTruthy();
+  expect(board["A"]["5"]["ship"]).toBeTruthy();
+  expect(board["A"]["6"]["ship"]).toBeFalsy();
 });
 
-// test("place ships horizontally", () => {
-//   let myBoard = gameboard();
-//   myBoard.placeShip("horizontal", "battleship", "F", "7");
-//   let board = myBoard.getBoard();
-//   expect(board["E"]["7"]["ship"]).toBeFalsy();
-//   expect(board["F"]["7"]["ship"]).toBeTruthy();
-//   expect(board["G"]["7"]["ship"]).toBeTruthy();
-//   expect(board["H"]["7"]["ship"]).toBeTruthy();
-//   expect(board["I"]["7"]["ship"]).toBeFalsy();
-// });
+test("place ships vertically that do not fit board", () => {
+  let myBoard = gameboard();
+  const shipName = "submarine";
+  myBoard.placeShip("vertical", shipName, "B", 8);
+  let board = myBoard.getBoard();
+
+  expect(board["B"]["7"]["ship"]).toBeFalsy();
+  expect(board["B"]["8"]["ship"]).toBeFalsy();
+  expect(board["B"]["9"]["ship"]).toBeFalsy();
+});
+
+test("place ships horizontally", () => {
+  let myBoard = gameboard();
+  const shipName = "battleship";
+  myBoard.placeShip("horizontal", shipName, "F", 7);
+  let board = myBoard.getBoard();
+
+  expect(board["E"]["7"]["ship"]).toBeFalsy();
+  expect(board["F"]["7"]["ship"]).toBeTruthy();
+  expect(board["G"]["7"]["ship"]).toBeTruthy();
+  expect(board["H"]["7"]["ship"]).toBeTruthy();
+  expect(board["I"]["7"]["ship"]).toBeTruthy();
+  expect(board["J"]["7"]["ship"]).toBeFalsy();
+});
+
+test("place ships horizontally that do not fit board", () => {
+  let myBoard = gameboard();
+  const shipName = "carrier";
+  myBoard.placeShip("horizontal", shipName, "G", 1);
+  let board = myBoard.getBoard();
+
+  expect(board["F"]["1"]["ship"]).toBeFalsy();
+  expect(board["G"]["1"]["ship"]).toBeFalsy();
+  expect(board["H"]["1"]["ship"]).toBeFalsy();
+  expect(board["I"]["1"]["ship"]).toBeFalsy();
+  expect(board["J"]["1"]["ship"]).toBeFalsy();
+});
