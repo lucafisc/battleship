e066e61459765752c600442eaaffad453827b0cd
lucafisc <lucafischer@fastmail.com>
2022-08-16 17:53:14 +0200

    Prevent ships from being placed next to each other

--- src/modules/game-control.js
+++ src/modules/game-control.js
@@ -4,7 +4,7 @@ import { player } from "./player-factory";
 import { newShip } from "./ship-factory";
 let human;
 let cpu;
-const shipsArray = [5, 4, 3, 3, 2];
+const shipsArray = [5, 4, 3, 3, 2, 2, 1, 1];
 
 export const playersStorage = () => {
   return [human, cpu];
@@ -58,11 +58,19 @@ const freeSpace = (props, board) => {
       freeSpace = false;
     } else if (isThereShipH(props, board)) {
       freeSpace = false;
+    } else if (isThereShipAroundH(props, board)) {
+      freeSpace = false;
+    } else if (isThereShipOnEdgesH(props, board)) {
+      freeSpace = false;
     }
   } else {
     if (isShipTooLongV(props)) {
       freeSpace = false;
     } else if (isThereShipV(props, board)) {
+      freeSpace = false;
+    } else if (isThereShipAroundV(props, board)) {
+      freeSpace = false;
+    } else if (isThereShipOnEdgesV(props, board)) {
       freeSpace = false;
     }
   }
@@ -114,6 +122,131 @@ function isThereShipV(props, board) {
       isThereShip = true;
     }
     shipCell += 10;
+  }
+  return isThereShip;
+}
+
+function isThereShipAroundV(props, board) {
+  let isThereShip = false;
+  let shipCell = props.cell;
+  const lastCellDigitStr = String(shipCell).slice(-1);
+  const lastCellDigitNum = Number(lastCellDigitStr);
+
+  for (let i = 0; i < props.length; i++) {
+    if (lastCellDigitNum > 0) {
+      if (board[shipCell - 1] !== "water") {
+        isThereShip = true;
+      }
+    }
+    if (lastCellDigitNum < 9) {
+      if (board[shipCell + 1] !== "water") {
+        isThereShip = true;
+      }
+    }
+
+    shipCell += 10;
+  }
+  return isThereShip;
+}
+
+function isThereShipAroundH(props, board) {
+  let isThereShip = false;
+
+  for (let i = 0; i < props.length; i++) {
+    if (props.cell >= 10) {
+      if (board[props.cell + i - 10] !== "water") {
+        isThereShip = true;
+      }
+    }
+    if (props.cell < 90) {
+      if (board[props.cell + i + 10] !== "water") {
+        isThereShip = true;
+      }
+    }
+  }
+  return isThereShip;
+}
+
+function isThereShipOnEdgesH(props, board) {
+  let isThereShip = false;
+  let shipCell = props.cell;
+  const lastCellDigitStr = String(shipCell).slice(-1);
+  const lastCellDigitNum = Number(lastCellDigitStr);
+
+  if (lastCellDigitNum > 0) {
+    const modififer = -1;
+    isThereShip = checkEdgesH(props, board, isThereShip, modififer);
+  }
+
+  if (lastCellDigitNum < 9) {
+    const modififer = props.length;
+    isThereShip = checkEdgesH(props, board, isThereShip, modififer);
+  }
+
+  return isThereShip;
+}
+
+function checkEdgesH(props, board, isThereShip, j) {
+  if (props.cell >= 10) {
+    if (board[props.cell + j - 10] !== "water") {
+      isThereShip = true;
+    }
+  }
+  if (board[props.cell + j] !== "water") {
+    isThereShip = true;
+  }
+  if (props.cell < 90) {
+    if (board[props.cell + j + 10] !== "water") {
+      isThereShip = true;
+    }
+  }
+  return isThereShip;
+}
+
+function isThereShipOnEdgesV(props, board) {
+  let isThereShip = false;
+  let shipCell = props.cell;
+  const lastCellDigitStr = String(shipCell).slice(-1);
+  const lastCellDigitNum = Number(lastCellDigitStr);
+
+  if (props.cell >= 10) {
+    const modififer = -10;
+    isThereShip = checkEdgesV(
+      lastCellDigitNum,
+      board,
+      props,
+      isThereShip,
+      modififer
+    );
+  }
+
+  if (props.cell < 90) {
+    const modififer = props.length * 10;
+    isThereShip = checkEdgesV(
+      lastCellDigitNum,
+      board,
+      props,
+      isThereShip,
+      modififer
+    );
+  }
+
+  return isThereShip;
+}
+
+function checkEdgesV(lastCellDigitNum, board, props, isThereShip, j) {
+  if (lastCellDigitNum > 0) {
+    if (board[props.cell - 1 + j] !== "water") {
+      isThereShip = true;
+    }
+  }
+  if (board[props.cell - 10] !== "water") {
+    isThereShip = true;
+  }
+  if (lastCellDigitNum < 9) {
+    if (board[props.cell + 1 + j] !== "water") {
+      isThereShip = true;
+    }
   }
   return isThereShip;
 }
